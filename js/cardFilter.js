cf32530cec2ad1dae5d9ee46f317266e32a853bf
Denis Antonov <antonov25041992@yandex.ru>
2021-10-14 17:11:39 +0300

    hotfix

--- js/cardFilter.js
+++ js/cardFilter.js
@@ -1,34 +1,32 @@
-jQuery(function ($) {
-    var cardsFilters = document.querySelectorAll(".card__filter");
-    cardsFilters.forEach(cardFilter => {
-        if (cardFilter.innerText === "Группы") {
+var cardsFilters = document.querySelectorAll(".card__filter");
+cardsFilters.forEach(cardFilter => {
+    // if (cardFilter.innerText === "Группы") {
+    //     cardFilter.classList.add('groups');
+    // }
+
+    switch (cardFilter.innerText) {
+        case "Группы":
             cardFilter.classList.add('groups');
-        }
+            break;
 
-        switch (cardFilter.innerText) {
-            case "Группы":
-                cardFilter.classList.add('groups');
-                break;
+        case "Студия":
+            cardFilter.classList.add('studio');
+            break;
 
-            case "Студия":
-                cardFilter.classList.add('studio');
-                break;
+        case "Академия":
+            cardFilter.classList.add('academy');
+            break;
 
-            case "Академия":
-                cardFilter.classList.add('academy');
-                break;
+        case "Кэмпы":
+            cardFilter.classList.add('camps');
+            break;
 
-            case "Кэмпы":
-                cardFilter.classList.add('camps');
-                break;
+        case "Онлайн":
+            cardFilter.classList.add('online');
+            break;
 
-            case "Онлайн":
-                cardFilter.classList.add('online');
-                break;
-
-            default:
-                cardFilter.classList.add('groups');
-                break;
-        }
-    });
+        default:
+            cardFilter.classList.add('groups');
+            break;
+    }
 });
--- js/transitionToAnchor.js
+++ js/transitionToAnchor.js
@@ -1,18 +1,19 @@
+const anchorToTop = document.querySelector("#toTop");
 
-jQuery(function ($) {
-    $(document).ready(function () {
-        $(".description__button").on("click", function (event) {
-            event.preventDefault();
-            var id = $(this).attr('href');
-            var top = $(id).offset().top - $(".header").height();
-            $('body,html').animate({ scrollTop: top }, 500);
-        });
-
-        $(".description__nav .nav__link").on("click", function (event) {
-            event.preventDefault();
-            var id = $(this).attr('href');
-            var top = $(id).offset().top - $(".header").height();
-            $('body,html').animate({ scrollTop: top }, 500);
-        });
+anchorToTop.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
     });
+});
+
+// Show Anchor
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 1000) {
+        anchorToTop.classList.remove('anchor_hidden');
+    } else {
+        anchorToTop.classList.add('anchor_hidden');
+    }
 });
